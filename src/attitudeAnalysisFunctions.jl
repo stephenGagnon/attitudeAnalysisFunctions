60537008454c822d1c952ef5e4255e97b1400871
Stephen Gagnon <srgagnon@buffalo.edu>
2021-04-26 13:34:59 -0400

    updated functions so that main optimization function can run PSO or GB algorithms

--- src/attitudeAnalysisFunctions.jl
+++ src/attitudeAnalysisFunctions.jl
@@ -1,11 +1,14 @@
 module attitudeAnalysisFunctions
 
-using lightCurveOptimization
 using attitudeFunctions
+using lightCurveOptimization
+using Distributed
+using SharedArrays
+using Infiltrator
 
-export attitudePSO_Main randomAttAnalysis
+export attitudeOptimizer, randomAttAnalysis
 
-function attitudePSO_Main(trueAttitude, params :: PSO_parameters,
+function attitudeOptimizer(trueAttitude, params :: Union{PSO_parameters,GB_parameters},
         options = optimizationOptions() :: optimizationOptions, a = 1.0 , f = 1.0;
         object = "simple satellite", scenario = "simple scenario",
         initialParticleDistribution = 0)
@@ -38,30 +41,41 @@ function attitudePSO_Main(trueAttitude, params :: PSO_parameters,
 
     costFunc = costFuncGen(sat,scen,A,options,a,f)
 
-    if cmp(options.initMethod,"random") == 0
-        xinit = randomAtt(params.N,options.Parameterization,options.vectorizeOptimization)
-    elseif cmp(options.initMethod,"specified") == 0
-        xinit = initialParticleDistribution
-    else
-        error("Please provide valid particle initialization method")
-    end
 
-    if options.useMPSO
+    if options.algorithm == "MPSO"
+
+        if options.initMethod == "random"
+            xinit = randomAtt(params.N,options.Parameterization,options.vectorizeOptimization)
+        elseif coptions.initMethod == "specified"
+            xinit = initialParticleDistribution
+        else
+            error("Please provide valid particle initialization method")
+        end
         results = MPSO_cluster(xinit,costFunc,params)
-    else
+    elseif options.algorithm == "PSO_cluster"
+
+        if options.initMethod == "random"
+            xinit = randomAtt(params.N,options.Parameterization,options.vectorizeOptimization)
+        elseif coptions.initMethod == "specified"
+            xinit = initialParticleDistribution
+        else
+            error("Please provide valid particle initialization method")
+        end
+
         results = PSO_cluster(xinit,costFunc,params)
-        if options.customTypes
+        if options.vectorizeOptimization
             results = Convert_PSO_results(results,options.Parameterization,a,f)
         end
+    elseif options.algorithm == "SPQ"
+
     end
 
     return optimizationResults(results, sat, satFull, scen, params, trueAttitude, options)
 end
 
-function randomAttAnalysis(N :: Int64, attType, params :: PSO_parameters,
+function randomAttAnalysis(N :: Int64, params :: PSO_parameters,
         options = optimizationOptions() :: optimizationOptions, a = 1.0 , f = 1.0;
-        object = "simple satellite", scenario = "simple scenario",
-        initialParticleDistribution = 0)
+        object = "simple satellite", scenario = "simple scenario")
 
     if cmp(object,"simple satellite") == 0
         (sat, satFull) = simpleSatellite(vectorized = options.vectorizeCost)
@@ -77,23 +91,48 @@ function randomAttAnalysis(N :: Int64, attType, params :: PSO_parameters,
         'simple scenario'")
     end
 
+    trueAttitudes = randomAtt(N,DCM,options.vectorizeOptimization)
 
-    if typeof(trueAttitude) == Array{Float64,2}
-        A = trueAttitude
-    elseif typeof(trueAttitude) == Union{MRP,GRP,quaternion,DCM}
-        A = any2A(trueAttitude)
-    elseif (typeof(trueAttitude) == Array{Float64,1}) & (length(trueAttitude) == 4)
-        A = q2A(trueAttitude)
-    elseif (typeof(trueAttitude) == Array{Float64,1}) & (length(trueAttitude) == 3)
-        A = p2A(trueAttitude,a,f)
-    else
-        error("please provide a valid true attitude")
+    # resultsFull = SharedArray{PSO_results,1}(undef,N)
+    resultsFull = Array{PSO_results,1}(undef,N)
+
+    for i = 1:N
+
+        A = trueAttitudes[i]
+        costFunc = costFuncGen(sat,scen,A,options,a,f)
+
+        if options.algorithm == "MPSO"
+
+            if options.initMethod == "random"
+                xinit = randomAtt(params.N,options.Parameterization,options.vectorizeOptimization)
+            elseif coptions.initMethod == "specified"
+                xinit = initialParticleDistribution
+            else
+                error("Please provide valid particle initialization method")
+            end
+            results = MPSO_cluster(xinit,costFunc,params)
+
+        elseif options.algorithm == "PSO_cluster"
+
+            if options.initMethod == "random"
+                xinit = randomAtt(params.N,options.Parameterization,options.vectorizeOptimization)
+            elseif coptions.initMethod == "specified"
+                xinit = initialParticleDistribution
+            else
+                error("Please provide valid particle initialization method")
+            end
+
+            results = PSO_cluster(xinit,costFunc,params)
+            if options.vectorizeOptimization
+                results = Convert_PSO_results(results,options.Parameterization,a,f)
+            end
+
+        end
+
+        resultsFull[i] = results
     end
 
-    costFunc = costFuncGen(sat,scen,A,options,a,f)
-
-
-
+    return optimizationResults(resultsFull, sat, satFull, scen, params, trueAttitudes, options)
 end
 
 end # module
